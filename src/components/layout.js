0ff4baa5f501be36a3434526aee6188c02c7d7f1
bobaekang <bobaekang@uchicago.edu>
2019-06-08 21:30:20 -0500

    add style

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,31 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+// material-ui
+import Container from '@material-ui/core/Container'
+import { withStyles } from '@material-ui/core/styles'
+
 // components
 import Header from "./header"
 
 // styles
 import "../styles/layout.css"
 
-const Layout = ({ children, fullpageSection, onSectionChange }) => {
+const styles = theme => ({
+	site: {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+  },
+  siteMain: {
+    flexGrow: "1"
+  },
+  footer: {
+    margin: "5px"
+  }
+})
+
+const Layout = ({ children, classes, fullpageSection, onSectionChange }) => {
   const handleSectionChange = index => onSectionChange(index)
 
   return ( 
@@ -23,19 +41,21 @@ const Layout = ({ children, fullpageSection, onSectionChange }) => {
         }
       `}
       render={data => (
-        <>
+        <div className={classes.site}>
           <Header
             siteTitle={data.site.siteMetadata.title}
             fullpageSection={fullpageSection}
             onSectionChange={index => handleSectionChange(index)}
           />
-            <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <main className={classes.siteMain}>{children}</main>
+            <footer className={classes.footer}>
+              <Container maxWidth="md">
+                © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+              </Container>
             </footer>
-        </>
+        </div>
       )}
     />
   )
@@ -45,4 +65,4 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default withStyles(styles)(Layout)
