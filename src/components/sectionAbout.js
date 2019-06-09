556fe5b4872cb2abab2111a2d91e8a3e0f2d9770
bobaekang <bobaekang@uchicago.edu>
2019-06-08 22:52:15 -0500

    add social icons

--- src/components/sectionAbout.js
+++ src/components/sectionAbout.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { loadCSS } from 'fg-loadcss';
+import clsx from 'clsx'
 
 // material ui
 import { withStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import Icon from '@material-ui/core/Icon'
+import EmailIcon from '@material-ui/icons/Email'
 
 // components
 import Image from './image.js'
@@ -14,15 +18,35 @@ import colors from '../styles/colors'
 const styles = theme => ({
   sectionTitle: {
     color: colors.red
+  },
+  socialList: {
+    textAlign: 'center'
+  },
+  social: {
+    margin: '20px 5px'
   }
 })
 
-const SectionAbout = ({ classes }) => (
+const SectionAbout = ({ classes }) => {
+  React.useEffect(() => {
+    loadCSS(
+      'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+      document.querySelector('#font-awesome-css'),
+    );
+  }, []);
+
+  return (
   <Container maxWidth="lg">
     <Grid container spacing={3}>
-      <Grid item xs={12} sm={3}>
+      <Grid item xs={12} sm={3} md={2}>
         <Image />
+        <div className={classes.socialList}>
+          <Icon className={clsx(classes.social, 'fab fa-github')} />
+          <Icon className={clsx(classes.social, 'fab fa-linkedin')} />
+          <EmailIcon className={classes.social}/>
+        </div>
       </Grid>
+
       <Grid item xs={12} sm={6}>
         <h2 className={classes.sectionTitle}>Hello World</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum officiis itaque doloribus placeat optio nihil ab nostrum maxime odio fugiat.</p>
@@ -30,6 +54,6 @@ const SectionAbout = ({ classes }) => (
       </Grid>
     </Grid>
   </Container>
-)
+)}
 
 export default withStyles(styles)(SectionAbout)
