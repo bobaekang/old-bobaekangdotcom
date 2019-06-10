2a7df34a97f5cae8c8ae4f3d3c891dd5406a93c5
bobaekang <bobaekang@uchicago.edu>
2019-06-09 19:24:52 -0500

    add link to prev/next

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 // material ui
 import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import { withStyles } from '@material-ui/core/styles'
 
 // components
@@ -29,14 +32,25 @@ const styles = theme => ({
   date: {
     marginBottom: "1em",
     color: colors.darkgrey
+  },
+  navIcon: {
+    position: "relative",
+    top: theme.spacing.unit,
+  },
+  navLink: {
+    color: colors.blue,
+    '&:hover': {
+      color: colors.red
+    }
   }
 });
 
 
-const blogPost = ({ classes, data }) => {
+const blogPost = ({ classes, data, pageContext }) => {
   const [section, setSection] = useState(2)
   const post = data.markdownRemark
-  
+  const { next, prev } = pageContext
+
   return (
     <Layout
       fullpageSection={section}
@@ -46,7 +60,48 @@ const blogPost = ({ classes, data }) => {
       <Container className={classes.blog} maxWidth="md">
         <h2>{post.frontmatter.title}</h2>
         <div className={classes.date}>{post.frontmatter.date}</div>
-        <div className={classes.article} dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div
+          className={classes.article}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Grid item xs={3}>
+            {
+              prev &&
+              <Link
+                className={classes.navLink}
+                to={prev.fields.slug}
+              >
+                <h4>
+                  <KeyboardArrowLeftIcon className={classes.navIcon}/>
+                  {prev.frontmatter.title}
+                </h4>
+              </Link>
+            }
+          </Grid>
+          <Grid item xs={3}>
+            {
+              next &&
+              <Link
+                className={classes.navLink}
+                style={{
+                  textAlign: 'right'
+                }}
+                to={next.fields.slug}
+              >
+                <h4>
+                  {next.frontmatter.title}
+                  <KeyboardArrowRightIcon className={classes.navIcon}/>
+                </h4>
+              </Link>
+            }
+          </Grid>
+        </Grid>
       </Container>
     </Layout>
   )
