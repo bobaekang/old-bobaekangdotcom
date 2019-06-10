f2da84819150d64d60a4fd544b7d006a65df39ff
bobaekang <bobaekang@uchicago.edu>
2019-06-09 22:04:05 -0500

    hide menu at sm down

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 // material ui
+import Hidden from '@material-ui/core/Hidden'
 import { withStyles } from '@material-ui/core/styles'
 
 // styles
@@ -56,66 +57,68 @@ const Header = ({ classes, fullpageSection, isBlog, onSectionChange }) => {
         </h1>
       }
       
-      <div className={classes.menu}>
-        <ul>
-          <li className={classes.menulist}>
-            <Link
-              to="/"
-              className={classes.menuItem}
-              onClick={() => onClickMenu(0)}
-            >
-              {
-                !isBlog &&
-                <span
-                  className={
-                    fullpageSection ===  0 ?
-                    classes.menuActiveItem :
-                    undefined
-                  }
-                >
-                  home
-                </span>
-              }
-            </Link>
-          </li>
-          <li className={classes.menulist}>
-            <Link
-              to="/"
-              className={classes.menuItem}
-              onClick={() => onClickMenu(1)}
-            >
-              {
-                !isBlog &&
-                <span
-                  className={
-                    fullpageSection === 1 ?
-                    classes.menuActiveItem :
-                    undefined
-                  }
-                >
-                  about
-                </span>
-              }
-            </Link>
-          </li>
-          <li className={classes.menulist}>
-            <Link
-              to={isBlog ? "/blog" :  "/"}
-              className={classes.menuItem}
-              onClick={() => onClickMenu(2)}
-            >
-              <span
-                className={
-                  fullpageSection === 2 ?
-                  classes.menuActiveItem :
-                  undefined}
+      <Hidden smDown>
+        <div className={classes.menu}>
+          <ul>
+            <li className={classes.menulist}>
+              <Link
+                to="/"
+                className={classes.menuItem}
+                onClick={() => onClickMenu(0)}
               >
-                blog
-              </span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+                {
+                  !isBlog &&
+                  <span
+                    className={
+                      fullpageSection ===  0 ?
+                      classes.menuActiveItem :
+                      undefined
+                    }
+                  >
+                    home
+                  </span>
+                }
+              </Link>
+            </li>
+            <li className={classes.menulist}>
+              <Link
+                to="/"
+                className={classes.menuItem}
+                onClick={() => onClickMenu(1)}
+              >
+                {
+                  !isBlog &&
+                  <span
+                    className={
+                      fullpageSection === 1 ?
+                      classes.menuActiveItem :
+                      undefined
+                    }
+                  >
+                    about
+                  </span>
+                }
+              </Link>
+            </li>
+            <li className={classes.menulist}>
+              <Link
+                to={isBlog ? "/blog" :  "/"}
+                className={classes.menuItem}
+                onClick={() => onClickMenu(2)}
+              >
+                <span
+                  className={
+                    fullpageSection === 2 ?
+                    classes.menuActiveItem :
+                    undefined}
+                >
+                  blog
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Hidden>
     </header>
   )
 }
