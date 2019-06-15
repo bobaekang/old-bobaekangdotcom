e27aa797940baa214b4d358b2127db0c92e094b7
bobaekang <bobaekang@uchicago.edu>
2019-06-15 10:03:30 -0500

    use grid; show menu on /blog

--- src/components/header.js
+++ src/components/header.js
@@ -3,38 +3,41 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 // material ui
+import Grid from '@material-ui/core/Grid'
 import Hidden from '@material-ui/core/Hidden'
 import { withStyles } from '@material-ui/core/styles'
 
 // styles
 import colors from '../styles/colors'
+import { Container } from "@material-ui/core";
 
 const styles = {
+  header: {
+    position:'absolute',
+    width:'100%',
+    zIndex:'99',
+  },
+  container: {
+    padding: '0'
+  },
   logo: {
-    position: 'absolute',
-    left: '10%',
-    top: '2%',
+    marginBottom: '0',
     backgroundColor: '#FF240E',
     color: 'white',
-    textDecoration: 'none',
-    zIndex: '99'
-  },
-  menu: {
-    position: 'absolute',
-    right: '10%',
-    top: '2%',
-    zIndex: '99'
+    textDecoration: 'none'
   },
   menulist: {
     float: 'left',
-    listStyleType: 'none'
+    listStyleType: 'none',
+    marginTop: '2%'
   },
   menuItem: {
     color: colors.blue,
     fontFamily: 'Ubuntu, san-serif',
     fontSize: '1.3em',
-    fontWeight: '700',    
-    padding: '1em',
+    fontWeight: '700',
+    paddingLeft: '1em',
+    paddingRight: '1em',
     textDecoration: 'none',
     '&:hover': {
       color: colors.red
@@ -49,76 +52,95 @@ const Header = ({ classes, fullpageSection, isBlog, onSectionChange }) => {
   const onClickMenu = index => onSectionChange(index)
   
   return (
-    <header>
-      {
-        fullpageSection > 0 && 
-        <h1>
-          <Link to="/" className={classes.logo}>bobae kang</Link>
-        </h1>
-      }
-      
-      <Hidden smDown>
-        <div className={classes.menu}>
-          <ul>
-            <li className={classes.menulist}>
-              <Link
-                to="/"
-                className={classes.menuItem}
-                onClick={() => onClickMenu(0)}
-              >
-                {
-                  !isBlog &&
-                  <span
-                    className={
-                      fullpageSection ===  0 ?
-                      classes.menuActiveItem :
-                      undefined
-                    }
+    <header className={classes.header}>
+      <Container className={classes.container} maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+        >
+          <Grid item>
+            {
+              fullpageSection > 0 && 
+              <h1 style={{margindBottom:'0'}}>
+                <Link to="/" className={classes.logo}>bobae kang</Link>
+              </h1>
+            }
+          </Grid>
+
+          <Grid item>
+            <Hidden smDown>
+              <ul>
+                <li className={classes.menulist}>
+                  <Link
+                    to="/"
+                    className={classes.menuItem}
+                    onClick={() => onClickMenu(0)}
                   >
-                    home
-                  </span>
-                }
-              </Link>
-            </li>
-            <li className={classes.menulist}>
-              <Link
-                to="/"
-                className={classes.menuItem}
-                onClick={() => onClickMenu(1)}
-              >
-                {
-                  !isBlog &&
-                  <span
-                    className={
-                      fullpageSection === 1 ?
-                      classes.menuActiveItem :
-                      undefined
+                    {
+                      !isBlog &&
+                      <span
+                        className={
+                          fullpageSection ===  0 ?
+                          classes.menuActiveItem :
+                          undefined
+                        }
+                      >
+                        home
+                      </span>
                     }
+                  </Link>
+                </li>
+                <li className={classes.menulist}>
+                  <Link
+                    to="/"
+                    className={classes.menuItem}
+                    onClick={() => onClickMenu(1)}
                   >
-                    about
-                  </span>
-                }
-              </Link>
-            </li>
-            <li className={classes.menulist}>
-              <Link
-                to={isBlog ? "/blog" :  "/"}
-                className={classes.menuItem}
-                onClick={() => onClickMenu(2)}
-              >
-                <span
-                  className={
-                    fullpageSection === 2 ?
-                    classes.menuActiveItem :
-                    undefined}
-                >
-                  blog
-                </span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </Hidden>
+                    {
+                      !isBlog &&
+                      <span
+                        className={
+                          fullpageSection === 1 ?
+                          classes.menuActiveItem :
+                          undefined
+                        }
+                      >
+                        about
+                      </span>
+                    }
+                  </Link>
+                </li>
+                <li className={classes.menulist}>
+                  <Link
+                    to={isBlog ? "/blog" :  "/"}
+                    className={classes.menuItem}
+                    onClick={() => onClickMenu(2)}
+                  >
+                    <span
+                      className={
+                        fullpageSection === 2 ?
+                        classes.menuActiveItem :
+                        undefined}
+                    >
+                      blog
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </Hidden>
+            
+            <Hidden mdUp>
+              {
+                isBlog &&              
+                <Link to="/blog" className={classes.menuItem}>
+                  <span className={classes.menuActiveItem}>blog</span>
+                </Link>
+              }
+            </Hidden>
+          </Grid>
+        </Grid>
+      </Container>
     </header>
   )
 }
