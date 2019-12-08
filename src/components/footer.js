1e680b5075a1a58f3c637d0cbbbee1edd95a7da1
bobaekang <hello@bobaekang.com>
2019-12-08 14:01:38 -0600

    refactor: simplify footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 // material ui
-import Container from '@material-ui/core/Container'
 import { withStyles } from '@material-ui/core/styles'
 
 // components
@@ -12,13 +11,12 @@ import colors from '../styles/colors'
 
 const styles = {
   footer: {
-    lineHeight: 'normal',
-    paddingTop: ".5rem",
-    paddingBottom: ".1rem",
-    fontSize: "0.8rem",
-    textAlign: 'center',
-    color: colors.darkgrey,
     backgroundColor: '#f9f9f9',
+    color: colors.darkgrey,
+    fontSize: "0.8rem",
+    paddingBottom: "0.1rem",
+    paddingTop: ".5rem",
+    textAlign: 'center',
     '& a': {
       '&:hover': {
         color: colors.red,
@@ -30,27 +28,25 @@ const styles = {
 
 const Footer = ({ classes }) => (
   <footer className={classes.footer}>
-    <Container maxWidth="lg">
-      <MySocial
-        namespace="footer"
-        styles={`
-          .footer-social-icon {
-            font-size: 1rem;
-            margin-left: .2rem;
-            margin-right: .2rem;
-          }
-        `}
-      />
-      <div>
-        © Bobae Kang {new Date().getFullYear()}, Powered by
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-        { `, ` }
-        <a href="https://material-ui.com/">Material UI</a>
-        { ` & ` }
-        <a href="https://alvarotrigo.com/fullPage/">FullPage.js</a>
-      </div>
-    </Container>
+    <MySocial
+      namespace="footer"
+      styles={`
+        .footer-social-icon {
+          font-size: 1rem;
+          margin-left: .2rem;
+          margin-right: .2rem;
+        }
+      `}
+    />
+    <div>
+      © Bobae Kang {new Date().getFullYear()}, Powered by
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      { `, ` }
+      <a href="https://material-ui.com/">Material UI</a>
+      { ` & ` }
+      <a href="https://alvarotrigo.com/fullPage/">FullPage.js</a>
+    </div>
   </footer>
 )
 
