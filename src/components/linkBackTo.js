1acf796d6f234a6d02e065d6fef0f85106bcc409
bobaekang <hello@bobaekang.com>
2020-01-11 16:49:20 -0600

    refactor: use a component for a link back to

---
+++ src/components/linkBackTo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+// material ui
+import { withStyles } from '@material-ui/core/styles'
+
+// styles
+import colors from '../styles/colors'
+
+const styles = {
+  back: {
+    color: colors.darkgrey,
+    '&:hover': {
+      color: colors.red,
+      textDecoration: 'underline',
+    },
+  },
+}
+
+const linkBackTo = ({ classes, to: { name, path } }) => (
+  <Link className={classes.back} to={path}>
+    ← {name}
+  </Link>
+)
+
+export default withStyles(styles)(linkBackTo)
