c9476771e33cff5cb4ac131b5bc03a7d8472e5a2
bobaekang <bkang.dev@gmail.com>
2022-06-19 20:01:13 -0500

    Customize tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      blue: '#66c3e5',
+      darkgrey: '#999',
+      red: '#FF240E',
+    },
     extend: {},
   },
   plugins: [],
