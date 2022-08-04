466a4c8e2f55bcae9dda3e0904b6761c6961e424
bobaekang <bkang.dev@gmail.com>
2022-08-03 21:37:28 -0500

    Add brand color/font to use to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,13 @@ module.exports = {
       blue: '#66c3e5',
       darkgrey: '#999',
       red: '#FF240E',
+      spotify: 'rgb(30, 215, 96)',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        spotify: ['Circular Spotify Text', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
